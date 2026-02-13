91099419c1a6e04a2bfa28f4e2a03fb942655634
Ben Richards <benjamindrichards@gmail.com>
2026-02-13 16:52:53 +1300

    Add missing GradientMapConfig definition.

---
+++ src/filters/typedefs/GradientMapConfig.js
@@ -0,0 +1,10 @@
+/**
+ * @typedef {object} Phaser.Types.Filters.GradientMapConfig
+ * @since 4.0.0
+ *
+ * @property {Phaser.Display.ColorRamp | Phaser.Types.Display.ColorRampConfig} [ramp] - The color ramp to use.
+ * @property {boolean} [dither=false] - Whether to dither the gradient output.
+ * @property {number[]} [color=[0, 0, 0, 0]] - Value to add to each channel.
+ * @property {number[]} [colorFactor=[0.3, 0.6, 0.1, 0]] - Factor to apply to each channel. Try to keep them summing to 1.
+ * @property {boolean} [unpremultiply=true] - Whether to unpremultiply color data before processing it.
+ */
